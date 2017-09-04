1587066f2559ee99951afa1463e664db5ea649c8
Rafa D. Latorre Lopez Villalta <sauco82@gmail.com>
2017-09-04 23:15:39 +0200

    obfuscate morse

--- spec/encodeMessageSpec.js
+++ spec/encodeMessageSpec.js
@@ -8,6 +8,13 @@ describe('encodeMessage', function(){
     expect(result).toEqual(expectedResult)
   })
 
+  it ('encodes and obfuscates messages', function() {
+    var result = encodeMessage('I AM IN TROUBLE', true),
+        expectedResult = '2/1A|B/2|A1/A|1A1|C|2A|A3|1A2|1';
+
+    expect(result).toEqual(expectedResult)
+  })
+
   it ('throws an error when the message is not a string', function(){
     expect(encodeMessage).toThrow('message must be an array')
   })
--- src/encodeMessage.js
+++ src/encodeMessage.js
@@ -39,7 +39,31 @@ var morseCode = {
   ",": "--..--"
 }
 
-module.exports = function encodeMessage(message){
+function obfuscateMorse(morseString) {
+  var charArray,
+      charCount,
+      lastChar;
+
+  if (!morseString) return;
+
+  charArray = morseString.split("")
+
+  charCount = charArray.reduce( function(acc, char) {
+    var last = acc.length - 1;
+
+    if (!acc.length || char != acc[last].char) {
+      acc.push({count: 1, char: char});
+      return acc;
+    }
+
+    acc[last].count++
+    return acc;
+   }, []);
+
+  return charCount.map( c => c.char == "-" ? String.fromCharCode(64 + c.count) : c.count ).join("");
+}
+
+module.exports = function encodeMessage(message, obfuscate = false){
   var msgArray,
       validChars = Object.keys(morseCode);
 
@@ -58,9 +82,11 @@ module.exports = function encodeMessage(message){
 
   // build morse code
   return msgArray.map( function(char, index) {
-    if (index == msgArray.length - 1) return morseCode[char];
-    if (msgArray[index+1] == " ") return morseCode[char] + "/";
-    if (msgArray[index] == " ") return "";
-    return morseCode[char] + "|";
+    var encodedChar = obfuscate ? obfuscateMorse(morseCode[char]) : morseCode[char];
+
+    if (index == msgArray.length - 1) return encodedChar;
+    if (msgArray[index+1] == " ") return encodedChar + "/";
+    if (char == " ") return "";
+    return encodedChar + "|";
   }).join("");
 }
